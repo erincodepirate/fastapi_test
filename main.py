87ec65be0fcd5e59b481ee488762efb78130b934
Erin Clark <erincodepirate@gmail.com>
2023-07-26 13:46:12 -0700

    Add path parameters with type

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ def index():
     return 'Hello there!'
 
 @app.get('/property/{id}')
-def property(id):
+def property(id:int):
     return f'This is a property page {id}'
+
+@app.get('/profile/{username}')
+def profile(username:str):
+    return {f'This is a profile page for user: {username}'}
 
 @app.get('/movies')
 def movies():
